e836d694c7576c516eae09618acdd0e90ab2373a
tigervanilla <piyushcool102@gmail.com>
2019-03-15 04:40:44 +0530

    file upload with multer

--- controllers/apiCtrl.js
+++ controllers/apiCtrl.js
@@ -1,7 +1,12 @@
+var multer=require('multer');
+
 module.exports={
     showHome:(req,res,next)=>{
-        res.render('index',{
-            'title':'Express',
-        });
+        res.render('index',{'title':'Express',});
+    },
+
+    uploadFile:(req,res,next)=>{
+        console.log("Uploaded Successfull with filename : ",req);
+        res.render('index',{'title':'File Upload Successful'});
     }
 }
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var multer=require('multer');
+var upload = multer({ dest: 'csv/' });
+
 var apiCtrl=require('./../controllers/apiCtrl');
 
 router.get('/home', apiCtrl.showHome);
+router.post('/upload',upload.single('myfile'),apiCtrl.uploadFile);
 
 module.exports = router;
